6e8012cb221ed6e052fee3efdbfa4d71a1de2218
Sukanta Naskar <sukanta.naskar@gmail.com>
2025-10-03 12:55:23 +0530

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Install Docker
 sudo apt install containerd -y
 sudo apt update
 sudo apt install docker.io -y
@@ -8,3 +9,8 @@ sudo systemctl restart docker
 sudo usermod -aG docker $USER
 docker --version
 # negrp docker
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
